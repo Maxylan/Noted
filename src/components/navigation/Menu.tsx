27636a2c6f1179586e34ffff395ae79e61c22d89
Max Olsson (Maxylan) <max100@live.se>
2023-10-05 21:05:18 +0200

    Added return to main menu button

--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import Note, { useNoteProps } from '../notes/Note';
 import Pages from '../../types/pages';
 import Home from './Home';
+import HouseIcon from '@mui/icons-material/House';
 /**
  * @license     MIT License
  * @author      Maxylan
@@ -22,15 +23,24 @@ export default function Menu(): JSX.Element {
     }, []);
 
     return (
-        <div className={['Menu', 'w-screen', 'h-screen', 'p-4', 'flex-block', 'md:w-fit', 'md:h-fit', 'm-auto', 'md:shadow-lg', 'md:rounded'].join(' ')}>
-            {((page) => {
-                switch(page) {
-                    case Pages.NewNote: return (<Note {...newNoteProps}/>);
-                    case Pages.Notes: return (<></>);
-                    case Pages.About: return (<></>);
-                    default: return (<Home currentPage={currentPage} setCurrentPage={setCurrentPage} />);
-                }
-            })(currentPage)}
-        </div>
+        <>
+            <div className={['Menu', 'relative', 'w-screen', 'h-screen', 'p-4', 'flex-block', 'md:w-fit', 'md:h-fit', 'm-auto', 'md:shadow-lg', 'md:rounded'].join(' ')}>
+                {((page) => {
+                    switch(page) {
+                        case Pages.NewNote: return (<Note {...newNoteProps}/>);
+                        case Pages.Notes: return (<></>);
+                        case Pages.About: return (<></>);
+                        default: return (<Home currentPage={currentPage} setCurrentPage={setCurrentPage} />);
+                    }
+                })(currentPage)}
+                {currentPage === Pages.Home|| (
+                    <span
+                        onClick={() => setCurrentPage(Pages.Home)}
+                        className={['absolute', 'bottom-4', 'left-4', 'p-4', 'rounded-full', 'shadow-lg', 'bg-third'].join(' ')}>
+                        <HouseIcon fontSize='large' className='text-primary'/>
+                    </span>
+                )}
+            </div>
+        </>
     )
 } 
